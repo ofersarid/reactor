e222934c254cc4c0a008cc2f75420c5bf6e448e6
ofersarid <ofersarid@gmail.com>
2019-06-27 12:01:53 +0300

    publish

--- src/cms/components/app/components/nav-bar/nav-bar.js
+++ src/cms/components/app/components/nav-bar/nav-bar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import { compose } from 'redux';
+import { Transition, animated } from 'react-spring/renderprops';
 import { connect } from 'react-redux';
 import { LogOutCircle } from 'styled-icons/boxicons-regular/LogOutCircle';
 import { Button } from '/src/cms/components';
@@ -9,26 +10,71 @@ import Auth from '/src/cms/components/auth';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-const NavBar = ({ logOut, uid, pathname }) => (
-  <div className={cx(styles.navBar)} >
-    <div className={styles.navBarTitle}>REACTOR</div>
-    {uid && pathname === '/cms/home' && (
-      <Button type="icon" className={cx(styles.toTheLeft, styles.btn)} onClick={logOut} >
-        <LogOutCircle />
-      </Button>
-    )}
-  </div >
-);
+class NavBar extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.titles = [springs => <animated.div className={cx(styles.navBarTitle)} style={springs} >REACTOR</animated.div >];
+    props.pages.forEach(p => {
+      this.titles.push(springs => <animated.div className={cx(styles.navBarTitle)} style={springs} >{p.name}</animated.div >);
+    });
+  }
+
+  resolvePageIndex(path) {
+    switch (true) {
+      case path === '/cms/login':
+      case path === '/cms/home':
+        return 0;
+      case path.includes('/cms/editor'):
+        return 1;
+      default:
+        return 0;
+    }
+  };
+
+  render() {
+    const { logOut, uid, pathname, prevPath } = this.props;
+    const pageIndex = this.resolvePageIndex(pathname);
+    const pageIndexPrev = this.resolvePageIndex(prevPath);
+    const direction = pageIndex > pageIndexPrev ? 'right' : 'left';
+
+    return (
+      <div className={cx(styles.navBar)} >
+        <Transition
+          native
+          reset
+          unique
+          items={pageIndex}
+          from={{ transform: `translateX(${direction === 'right' ? '0%' : '-100%'})`, opacity: 0 }}
+          enter={{ transform: 'translateX(-50%)', opacity: 1 }}
+          leave={{ transform: `translateX(${direction === 'right' ? '-100%' : '0%'})`, opacity: 0 }} >
+          {index => this.titles[index]}
+        </Transition >
+        {uid && pathname === '/cms/home' && (
+          <Button type="icon" className={cx(styles.toTheLeft, styles.btn)} onClick={logOut} >
+            <LogOutCircle />
+          </Button >
+        )}
+      </div >
+    );
+  }
+}
 
 NavBar.propTypes = {
   logOut: PropTypes.func.isRequired,
   uid: PropTypes.string,
   pathname: PropTypes.string.isRequired,
+  prevPath: PropTypes.string.isRequired,
+  pages: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 const mapStateToProps = state => ({
   uid: Auth.selectors.uid(state),
   pathname: Routes.selectors.pathname(state),
+  prevPath: Routes.selectors.prevPath(state),
+  pages: [{
+    name: 'FRAME',
+    id: '3',
+  }],
 });
 
 const mapDispatchToProps = dispatch => ({
