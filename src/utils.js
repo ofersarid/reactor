d57ca67789f81d4410415deb458e624b40d5ed2d
ofersarid <ofersarid@gmail.com>
2019-08-04 12:52:18 +0300

    publish

--- src/utils.js
+++ src/utils.js
@@ -22,12 +22,7 @@ export const validatePhone = phone => {
 };
 
 const validateLink = str => {
-  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  const pattern = new RegExp(/[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi); // fragment locator
   return !!pattern.test(str);
 };
 
